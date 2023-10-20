761098f19cb9813ef01b55f98c89b62a60302d2e
brvnfr <ymmk93@gmail.com>
2023-10-19 22:05:00 -0300

    [enchance] header design

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,47 +1,59 @@
 'use client'
-
-
 import { SafeUser } from '@/types/type'
 import { signOut } from 'next-auth/react'
 import Link from 'next/link'
 import Image from 'next/image'
 import React from 'react'
 import { Container, Paper, Button, Text, Group } from '@mantine/core';
+import { IconPlus } from '@tabler/icons-react';
 import ToggleTheme from '@/components/ToggleTheme/ToggleTheme'
+
 interface UserMenuProps {
-    currentUser: SafeUser | null
-  }
+  currentUser: SafeUser | null
+}
 
-
-export default function Navbar({currentUser}:UserMenuProps) {
-
-
+export default function Navbar({currentUser}: UserMenuProps) {
   return (
     <Paper shadow="xs" style={{ padding: '1rem' }}>
-    <Container>
-      <Group justify="space-between" align="center">
-        <Text size="xl">
-        <h1><strong>Dom Pixel Blog</strong></h1>
+      <Container>
+        <Group justify="space-between" align="center">
+          <Text size="xl">
+            <h1><strong>Dom Pixel Blog</strong></h1>
+          </Text>
 
-        </Text>
+          <Group>
+            <Link href="/">Inicio</Link>
+            <Link href="/create">
+              <Button variant="filled" color="green">
+                <IconPlus /> Publicar
+              </Button>
+            </Link>
 
-        <Group>
-          <Link href="/">Inicio</Link>
-          <Link href="/create">Publicar</Link>
-          {currentUser ? (
-            <Button variant="filled" onClick={() => signOut()}>
-              Logout
-            </Button>
-          ) : (
+            {currentUser ? (
+              <>
+                <Button variant="filled" onClick={() => signOut()}>
+                  Sair
+                </Button>
+              </>
+            ) : (
+              <>
+                <Link href="/register">
+                  <Button variant="filled">
+                    Registrar
+                  </Button>
+                </Link>
+                <Link href="/login">
+                  <Button variant="filled">
+                    Entrar
+                  </Button>
+                </Link>
+              </>
+            )}
 
-            <Link href="/register">
-              Cadastrar
-              </Link>
-          )}
-          <ToggleTheme />
+            <ToggleTheme />
+          </Group>
         </Group>
-      </Group>
-    </Container>
-  </Paper>
+      </Container>
+    </Paper>
   )
 }
