c491c7ce6d8aef9a02a07fc908741d203c580897
Bruno Onofre <85579006+brvnfr@users.noreply.github.com>
2023-10-19 23:59:46 -0300

    Update SingleBlog.tsx
    [update] singleblog

--- components/SingleBlog/SingleBlog.tsx
+++ components/SingleBlog/SingleBlog.tsx
@@ -67,7 +67,7 @@ export default function SingleBlog({key,data,currentUser}:BlogProps) {
     
           <Card.Section className={classes.section}>
           </Card.Section>
-    
+          {data.userId === currentUser?.id && (
           <Group mt="xs">
             <Button onClick={() => router.push(`/blogs/${data.id}`)} radius="md" style={{ flex: 1 }}>
               Ver Mais
@@ -75,35 +75,9 @@ export default function SingleBlog({key,data,currentUser}:BlogProps) {
             <ActionIcon variant="default" radius="md" size={36}>
               <IconTrash onClick={onDelete} className={classes.trash} stroke={1.5} />
             </ActionIcon>
-          </Group>
+          </Group> )}
         </Card>
         </div>
       );
-
-    
     }
     
-    
-      // return (
-      //   <div className="w-[1100px] border-2 p-4">
-      //       <div className="">
-      //           <div className="flex gap-2 justify-between items-center">
-      //               <Image width={400} className="w-[500px] object-contain" height={300} src={data.imageSrc} alt="Blog Image" />
-    
-      //               <div className="w-[530px] flex flex-col gap-4 leading-[1.5]">
-      //               <h1>{data.name}</h1>
-      //               <span>{data.description}</span>
-      //               </div>
-      //           </div>
-      //       </div>
-    
-      //       {data.userId === currentUser?.id && (
-      //           <div className="flex items-center gap-4 mt-4">
-      //         <RiDeleteBin5Line onClick={onDelete} className=" cursor-pointer text-[1.5rem]"/>
-      //         <BsFillPencilFill onClick={() => router.push(`/blogs/${data.id}`)} className=" cursor-pointer text-[1.2rem]"/>
-      //         {/* <button className="bg-red-400 px-6 py-2" onClick={onDelete}>Delete</button> */}
-      //         {/* <button className="bg-yellow-400 px-6 py-2" onClick={() => router.push(`/blogs/${data.id}`)}>Edit</button> */}
-      //         </div>
-      //       )}
-      //   </div>
-      // )
